1ff6897c11adec987172aca2f63069dcf8b35c8f
Olol1234 <muhammadazfar0239@gmail.com>
2026-02-18 22:37:29 +0800

    feat: add combat action for turn base

---
+++ MiniRPG-Simulation-CPP/include/CombatAction.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class CombatAction
+{
+	Attack,
+	Defend,
+	Run
+};
--- MiniRPG-Simulation-CPP/include/CombatSystem.h
+++ MiniRPG-Simulation-CPP/include/CombatSystem.h
@@ -4,6 +4,8 @@
 #include "Definitions.h"
 #include "CombatStatsComponent.h"
 #include "CombatResult.h"
+#include "CombatTurn.h"
+#include "CombatAction.h"
 
 class CombatSystem
 {
@@ -11,5 +13,20 @@ public:
 	static CombatResult StartCombat(
 		CombatStatsComponent& playerStats,
 		const CharacterDefinition* enemyDef
+	);
+
+private:
+	static void StartTurn(CombatTurn turn);
+	static void EndTurn(CombatTurn turn);
+
+	static CombatAction ChoosePlayerAction();
+	static CombatAction ChooseEnemyAction();
+	static void ExecuteAction(
+		CombatAction action,
+		int& attackerHp,
+		int& defenderHp,
+		int attackerAttack,
+		int attackerDefense,
+		bool isPlayerAttacker
 	);
 };
